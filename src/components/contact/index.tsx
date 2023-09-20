dd2d823d5b8a672dfbe18c36db41e2377217af13
samirdourado <samir3500@gmail.com>
2023-09-20 11:16:39 -0300

    Feat: maps google

--- src/components/contact/index.tsx
+++ src/components/contact/index.tsx
@@ -25,12 +25,18 @@ const Contact = () => {
         />
         <h2>Casa Adesive</h2>
         <p>Travessa Dona Paula 13, São Paulo - SP, 01239-050, Brazil</p>
-        <p>11 99305-4964</p>        
-        <Button
-            className={styles.buttons__contacts__area}
-            icon={<MdPlace size={20}/>}
-            text={'Como Chegar'}
-        />
+        <p>11 99305-4964</p>
+        <iframe 
+          className={styles.maps__container}
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1537.7698695963406!2d-46.660656061175494!3d-23.552141979708296!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce583221c9ac81%3A0xc1d422cb9f0870b9!2sTv.%20Dona%20Paula%2C%2013%20-%20Higien%C3%B3polis%2C%20S%C3%A3o%20Paulo%20-%20SP%2C%2001239-050!5e0!3m2!1spt-BR!2sbr!4v1695158810116!5m2!1spt-BR!2sbr" 
+          width="600" 
+          height="450" 
+          // style="border:0;" 
+          // allowfullscreen="" 
+          loading="lazy" 
+          // referrerpolicy="no-referrer-when-downgrade"
+          >            
+        </iframe>
       </section>
     );
 };
